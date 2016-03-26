b3a39c1aec26742b9aeeca38a9694f70b8bd56a1
Stephane Recouvreur <stefan.recouvreur@gmail.com>
2016-03-26 14:55:15 +1100

    first commit

---
+++ collection/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    # return view for index
+    number = 6
+    thing = "Thing name"
+    return render(request, 'index.html', {
+        'number': number,
+        'thing': thing,
+    })
