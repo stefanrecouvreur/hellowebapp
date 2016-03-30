c58fb1b94ad0efd339e67da9bdd5fd21f2f3c06a
Stephane Recouvreur <stefan.recouvreur@gmail.com>
2016-03-30 15:25:40 +1100

    change in wsgi.py

--- hellowebapp/wsgi.py
+++ hellowebapp/wsgi.py
@@ -6,12 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellowebapp.settings_production")
 
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application) 
